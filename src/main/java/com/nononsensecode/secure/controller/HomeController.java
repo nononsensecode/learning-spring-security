80bee75c077a7451a410f76cb50844d30a908099
Kaushik Asokan <kaushikam@gmail.com>
2021-02-18 12:54:35 +0530

    Adding user and admin roles to the application

--- src/main/java/com/nononsensecode/secure/controller/HomeController.java
+++ src/main/java/com/nononsensecode/secure/controller/HomeController.java
@@ -9,4 +9,14 @@ public class HomeController {
     public String home() {
         return ("<h1>Welcome</h1>");
     }
+
+    @RequestMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User!</h1>");
+    }
+
+    @RequestMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin!</h1>");
+    }
 }
